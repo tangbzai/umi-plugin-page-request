a40540802c29dabb6190807d05e121a18e2fa98e
tangbzai <tan-jc@qq.com>
2024-10-11 17:00:32 +0800

    perf: createPageRequestMap阶段每个filePath指挥获取一遍ServicesList，无需处理列表有数据的情况

--- src/pageRequestMap.ts
+++ src/pageRequestMap.ts
@@ -28,14 +28,13 @@ function dirOrFileNormalization(dirOrFile?: string): string[] {
     .flatMap((dir) => dirOrFileNormalization(path.join(dirOrFile, dir)))
 }
 
-/** 列表去重后返回（含自身重复及与旧列表重复）*/
-function filterNoRepeat<T>(newList: T[], oldList: T[], keyFn: (value: T) => string | number) {
-  const oldKeyList = new Set(oldList.map(keyFn))
+/** 列表去重后返回 */
+function filterNoRepeat<T>(newList: T[], keyFn: (value: T) => string | number) {
   const newKeySet = new Set()
   return newList.filter((item) => {
     const key = keyFn(item)
     // 新列表出现过或旧列表中已存在则直接返回空
-    if (newKeySet.has(key) || oldKeyList.has(key)) return false
+    if (newKeySet.has(key)) return false
     newKeySet.add(key)
     return true
   })
@@ -51,8 +50,7 @@ export default function InitPageRequest(api: IApi) {
    * @param functionNameList
    * @returns 接口函数对象
    */
-  function getServiceList(filePath: string, functionNameList: string[]): RequestFunction[] {
-    const group = filePath.match(/@\/services\/([^/\\]*)/)?.[1]
+  function getServiceList(group: string, functionNameList: string[]): RequestFunction[] {
     if (!group || !servicesMap[group]) return []
     return functionNameList.flatMap((functionName) =>
       !!servicesMap[group][functionName] ? [servicesMap[group][functionName]] : []
@@ -73,20 +71,16 @@ export default function InitPageRequest(api: IApi) {
   function getDeclarationSourceAbsPath(declarationSource: string, filePath: string): string {
     if (!declarationSource.startsWith('.')) return aliasPathFormatAbsPath(declarationSource)
     // 拼接出当前文件引入的模块路径
-    // 使用正则表达式匹配最后一个反斜杠或正斜杠
-    const lastSeparator = filePath.match(/[/\\][^/\\]*$/)?.[0] // 获取匹配到的最后一个分隔符
-    if (lastSeparator) {
-      const dir = filePath.slice(0, -lastSeparator.length) // 截取路径至最后一个分隔符之前的部分
-      return path.join(dir, declarationSource)
-    }
-    return path.join(filePath, declarationSource)
+    const dir = filePath.replace(/[/\\][^/\\]*$/, '')
+    return path.join(dir, declarationSource)
   }
 
+  type ImportMapItemType = { source: string; localList: string[] }
   /** 模块 */
-  const fileImportsMap = new Map<string, ImportDeclaration[]>()
+  const fileImportsMap = new Map<string, ImportMapItemType[]>()
 
   /** 通过文件路径获取 ImportDeclaration */
-  function getImportDeclarationByFilePath(absPath: string): ImportDeclaration[] {
+  function getImportDeclarationByFilePath(absPath: string): ImportMapItemType[] {
     return fileImportsMap.get(absPathFormatAliasPath(absPath)) || []
   }
 
@@ -107,10 +101,8 @@ export default function InitPageRequest(api: IApi) {
 
       if (declaration.source.startsWith('@/services')) {
         return getServiceList(
-          declaration.source,
-          declaration.specifiers.flatMap((item) =>
-            'local' in item && !!item.local ? [item.local] : []
-          )
+          declaration.source.match(/@\/services\/([^/\\]*)/)?.[1],
+          declaration.localList
         )
       }
 
@@ -130,28 +122,33 @@ export default function InitPageRequest(api: IApi) {
     fileImportsMap.clear()
     if (!fileImports) return {}
     return Object.entries(fileImports)
-      .reduce<{ filePath: string; absPath: string }[]>((acc, [absPath, list]) => {
+      .flatMap(([absPath, list]) => {
         const filePath = absPathFormatAliasPath(absPath)
         const importDeclarationList = list?.filter(
           (declaration): declaration is ImportDeclaration =>
             declaration.type === 'ImportDeclaration' &&
             !new RegExp('^@?[a-zA-Z]').test(declaration.source)
         )
+
         // 组装 页面 / 组件 的 相对路径与 Import 依赖的映射关系
-        fileImportsMap.set(filePath, importDeclarationList)
+        fileImportsMap.set(
+          filePath,
+          importDeclarationList?.map((item) => ({
+            source: item.source,
+            localList: item.specifiers.flatMap((item) => (item.local ? [item.local] : [])),
+          })) || []
+        )
 
         // 从页面组件开始访问
-        if (/^@\/pages\/.*\.tsx$/.test(filePath)) acc.push({ filePath, absPath })
-        return acc
-      }, [])
+        if (/^@\/pages\/.*\.tsx$/.test(filePath)) return [{ filePath, absPath }]
+        return []
+      })
       .reduce<Record<string, RequestFunction[] | undefined>>((result, { filePath, absPath }) => {
         // 获取该页面引入的的接口列表并去重
-        const newFunctionList = filterNoRepeat(
+        result[filePath] = filterNoRepeat(
           getComponentServicesList(absPath),
-          result[filePath] || [],
           ({ method, url }) => `${method}-${url}`
         )
-        result[filePath] = [...(result[filePath] || []), ...newFunctionList]
         return result
       }, {})
   }
